c4672c544221d90fbc8668798d730f9830227c4b
august0Azevedo <augusto_pedroso@estudante.scs.senai.br>
2024-07-02 20:11:21 -0300

    teste

--- pousadaypua/src/main.js
+++ pousadaypua/src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+//bibliotecas bootstrap
+import { BootstrapVue3 } from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 const app = createApp(App);
 app.use(router);
+app.mount('#app');
+app.use(BootstrapVue3);
 app.mount('#app');
