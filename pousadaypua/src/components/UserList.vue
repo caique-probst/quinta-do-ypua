eb23ef135c5f311d54b30cde3eede009bbb56144
unknown <meucaique@gmail.com>
2024-07-02 19:02:02 -0300

    att em userlist, voltando com o botão de adicionar um usuario e att nos usuarios

--- pousadaypua/src/components/UserList.vue
+++ pousadaypua/src/components/UserList.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
     <h1>Usuários</h1>
+    <button @click="showPopup = true">Novo usuário</button>
     <div class="users-container">
       <UserItem v-for="user in users" :key="user.nome" :user="user" />
     </div>
+    <UserPopup v-if="showPopup" @close="showPopup = false">
+      <CadastroFuncionario @cadastrado="adicionarUsuario" />
+    </UserPopup>
   </div>
 </template>
 
 <script>
-import UserItem from './UserItem.vue'
+import UserItem from './UserItem.vue';
+import UserPopup from './UserPopup.vue';
+import CadastroFuncionario from './CadastroFuncionario.vue';
 
 export default {
   name: 'UserList',
   components: {
-    UserItem
+    UserItem,
+    UserPopup,
+    CadastroFuncionario
   },
   data() {
     return {
+      showPopup: false,
       users: [
         {
           id: 1,
@@ -34,6 +43,12 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    adicionarUsuario(usuario) {
+      this.users.push(usuario);
+      this.showPopup = false;
+    }
   }
 }
 </script>
@@ -43,5 +58,19 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 20px;
+}
+
+button {
+  margin-bottom: 10px;
+  padding: 10px;
+  font-size: 16px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #369f6e;
 }
 </style>
