ae213a8aa81c6d7dd3c8b2474c06ac6491a9e44d
unknown <meucaique@gmail.com>
2024-07-02 18:28:58 -0300

    Atualização na lista de usuários e adição do UserItem

---
+++ pousadaypua/src/components/UserItem.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="user-item" @click="showUserDetails(user.id)">
+      <div class="user-info">
+        <div class="user-info-item">
+          <p>Nome:</p>
+          <p>{{ user.nome }}</p>
+        </div>
+        <div class="user-info-item">
+          <p>Função:</p>
+          <p>{{ user.função }}</p>
+        </div>
+        <div class="user-info-item">
+          <p>Status:</p>
+          <p>{{ user.status }}</p>
+        </div>
+        <div class="user-info-item">
+          <p>Ações:</p>
+          <p>{{ user.ações }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserItem',
+    props: {
+      user: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      showUserDetails(id) {
+        this.$router.push({ name: 'UserDetails', params: { id } });
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .user-item {
+    border: 1px solid #ccc;
+    padding: 10px;
+    cursor: pointer;
+  }
+  
+  .user-item:hover {
+    background-color: rgba(255, 192, 67, 0.25);
+  }
+  
+  .user-info {
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  .user-info-item {
+    flex: 1;
+    text-align: center;
+    padding: 40px;
+  }
+  
+  .user-info-item p {
+    margin: 0;
+    padding-top: 15px;
+  }
+  </style>
+  
